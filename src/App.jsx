c24f3e71498c9097f0f6bd645f974e0808f4075b
Alapan Mukherjee <alapan.mukherjee35@gmail.com>
2025-03-03 16:18:23 +0000

    Update StartingPot component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Game from './components/Game';
 import StartingPot from './StartingPot';
 import WidowsAppHome from './WidowsAppHome';
 
+export const GameContext = createContext({});
+
 function App() {
+  const initialGameState = {
+    pointsInPot: 8000,
+    cover: 0,
+  };
+
+  const [gameState, setGameState] = useState(initialGameState);
+
+  const updateGameState = (newState) => {
+    setGameState(newState)
+  };
+
   return (
-    <div>
+    <GameContext.Provider value={{gameState, updateGameState}}>
       <Router>
         <Routes>
-          <Route path="/" element={<WidowsAppHome />} />
-          <Route path="/starting-pot" element={<StartingPot />} />
+          <Route path='/' element={<CatOrDog />} />
+          <Route path='/starting-pot' element={<StartingPot />} />
           <Route path="/game" element={<Game initialLives={5} iconType="cat" />} />
         </Routes>
       </Router>
-    </div>
+    </GameContext.Provider>
   );
 }
 
